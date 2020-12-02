7f13ee600835ddb127af21b26a8d3f8c8ecfc6d4
Sean Anggani <seanggani@gmail.com>
2020-12-01 21:52:07 -0500

    holler

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hola there!!!</h1>
+      <h1>Holler there!!!</h1>
       <hr />
       <MarketingApp />
     </div>
