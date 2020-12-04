86214f563183ee4220b1936467ba5799f10149e5
Sean Anggani <seanggani@gmail.com>
2020-12-03 20:22:08 -0500

    aloha

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,8 +1,8 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import { mount } from "marketing/MarketingApp"; //mount obtained from marketing function
+import React, { useRef, useEffect } from "react";
 
 export default () => {
-  const ref = useRef(null);
+  const ref = useRef(null); //accesses a mutable ref to the child
 
   useEffect(() => {
     mount(ref.current);
