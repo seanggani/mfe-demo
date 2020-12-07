31704cf4822e9635010c9777f71ba0ebb3c3fe8c
Sean Anggani <seanggani@gmail.com>
2020-12-07 13:11:54 -0500

    Revolutionary

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              New Home Page
+              Revolutionary Home Page
             </Typography>
             <Typography
               variant="h5"
